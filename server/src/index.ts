f8f657480df77b870b84aeaa0cf547c3899de8cd
Julian Jimenez <julianjimenez2128@gmail.com>
2025-04-07 00:58:27 -0500

    chore: Reorganized imports and improved middleware configuration in index.ts and added new routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,34 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import RegisterRoutes from "./routes/RegisterRoutes.ts";
+import LoginRoutes from "./routes/LoginRoutes.ts";
+import HomeRoutes from "./routes/HomeRoute.ts";
+import cors from "cors";
+import helmet from "helmet";
 
-dotenv.config(); // Lee variables de .env
+dotenv.config(); //read variables of .env
 
-const app = express();
+const app = express(); //create express app
+app.use(helmet()); //Middleware to protect the app of common attacks
+
+//Middleware para habilitar CORS
 const PORT = process.env.PORT || 3000;
+app.use(
+  cors({
+    origin: [process.env.ALLOWED_ORIGINS || "http://localhost:3000"], //allowed origins
+    credentials: true, //allow credentials
+    methods: ["GET", "POST", "PUT", "DELETE"], //allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], //allowed headers
+  })
+);
 
-app.use(express.json()); // Middleware para leer JSON
+app.use(express.json()); //middlware para teaches json
 
-app.get('/', (_req, res) => {
-  res.send('Hola Jefe Julian 🚀');
-});
+app.use("/", RegisterRoutes); //Registration routes
+app.use("/", LoginRoutes); //Login Routes
+app.use("/", HomeRoutes); //Home Routes
 
 app.listen(PORT, () => {
+  //start server
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
